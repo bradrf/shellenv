95dd32e829d8ee8ee2360e2a959eec16d57a1bae
bradrf <brad@gigglewax.com>
2015-08-28 13:01:54 -0700

    cache all volumes async to help speed processing

--- home/lib/python/simple_aws.py
+++ home/lib/python/simple_aws.py
@@ -48,13 +48,28 @@ class DnsRecord(object):
 
 ########################################
 class Volume(object):
-    def __init__(self, connection, device_name, block_device):
+    volumes = {}
+
+    @staticmethod
+    def set_volumes(volumes):
+        for v in volumes:
+            Volume.volumes[v.id] = v
+
+    @staticmethod
+    def get_volume(id, region=None):
+        volume = Volume.volumes.get(id)
+        if volume:
+            volume.update()
+        else:
+            volume = Volume.volumes[id] = Connections.get_connection(region).get_all_volumes(volume_ids=[id])[0]
+        return volume
+
+    def __init__(self, device_name, block_device):
         self.block_device = block_device
         self.id = block_device.volume_id
         self.device_name = device_name
         self.delete_on_termination = block_device.delete_on_termination
-        # fixme: this is likely the _wrong_ way to get the volume... and slow, too
-        self.boto = connection.get_all_volumes(volume_ids=[self.id])[0]
+        self.boto = Volume.get_volume(self.id)
         self.status = self.boto.status
 
     def wait_for(self, status, interval=3):
@@ -91,7 +106,7 @@ class Instance(object):
         except:
             self.name = self.public if self.public is not None else self.private
         self.tags_str = ','.join('%s=%s'%(k,re.sub(r'\s+','_',v)) for k,v in self.tags.iteritems())
-        self.volumes = [Volume(metadata.connection, n, d) for n,d in metadata.block_device_mapping.iteritems()]
+        self.volumes = [Volume(n, d) for n,d in metadata.block_device_mapping.iteritems()]
 
     def wait_for(self, state, interval=3):
         self.update()
@@ -153,8 +168,14 @@ def get_region_instances(region):
         print 'Unable to get instances in region:', region
         return []
 
+def get_region_volumes(region):
+    return get_connection(region).get_all_volumes()
+
 def get_instances():
     regions = [r.name for r in boto.ec2.regions() if r.name not in ['us-gov-west-1','cn-north-1']];
+    pool = multiprocessing.Pool(len(regions))
+    volumes = pool.map(get_region_volumes, regions)
+    Volume.set_volumes([val for subl in volumes for val in subl])
     pool = multiprocessing.Pool(len(regions))
     instances = pool.map(get_region_instances, regions)
     return [val for subl in instances for val in subl] # flatten
