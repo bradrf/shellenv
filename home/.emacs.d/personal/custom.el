c79375881853b2e6d272b53b71fff17205a784a2
bradrf <brad@gigglewax.com>
2017-01-01 16:03:49 -0800

    fix gettem; fix emacs spell prog

--- home/.emacs.d/personal/custom.el
+++ home/.emacs.d/personal/custom.el
@@ -5,7 +5,6 @@
  ;; If there is more than one, they won't work right.
  '(fill-column 100)
  '(helm-man-format-switches "%s")
- '(ispell-program-name "/usr/local/bin/aspell")
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
@@ -13,10 +12,5 @@
  '(prelude-guru nil)
  '(undo-tree-visualizer-diff t)
  '(vc-follow-symlinks t)
- '(whitespace-line-column nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+ '(whitespace-line-column nil)
  )
